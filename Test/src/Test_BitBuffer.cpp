3a11deb832ec8ed9fb88a528be57209480cb86d0
Reto <reto271@gmx.ch>
2020-03-12 16:39:33 +0100

    Add more test cases for BitBuffer
    - Add 34 single bit variables
    - Read partial data from BitBuffer
    - Add random numbers and read them back

--- Test/src/Test_BitBuffer.cpp
+++ Test/src/Test_BitBuffer.cpp
@@ -37,12 +37,20 @@ class BitBufferTest : public testing::Test
 protected:
     void SetUp() override
     {
-        //Create and initialize trace infrastructure
+        srand(time(NULL));
     }
 
     void TearDown() override
     {
         //Dump Trace buffer
+    }
+
+    void getRandomValue(uint32_t& value, uint16_t& bitWidth)
+    {
+        bitWidth = (rand() % 32) + 1;
+        value = rand() & 0xffffffff;
+        EXPECT_TRUE(0 < bitWidth);
+        EXPECT_TRUE(32 >= bitWidth);
     }
 
     //StrictMock<test::Mock_DummyDriver> m_dummyDriver;
@@ -76,20 +84,13 @@ TEST_F(BitBufferTest, setGet32BitsAtOnce)
 
     m_testBuffer.restartReading();
     uint32_t value = m_testBuffer.getBits(32);
+    EXPECT_EQ(0x12345678, value);
 
     m_testBuffer.restartReading();
-    uint32_t value8bits = m_testBuffer.getBits(4);
+    uint32_t value4bits = m_testBuffer.getBits(4);
+    EXPECT_EQ(value4bits, value & 0xf);
 
-
-    m_testBuffer.printContent();
-    std::cout << "val       : 0x" << std::hex << value << std::dec << std::endl;
-    std::cout << "val 8 bit : 0x" << std::hex << value8bits << std::dec << std::endl;
-    std::cout << "0xf & val : " << (0xf & value) << std::endl;
-    std::cout << "0xf & exp : " << (0xf & 0x12345678) << std::endl;
-
-
-
-    EXPECT_EQ(0x12345678, value);
+//    m_testBuffer.printContent();
 }
 
 TEST_F(BitBufferTest, set32BitsCheckBuffer)
@@ -124,21 +125,67 @@ TEST_F(BitBufferTest, set5ByteValues)
 }
 
 
-TEST_F(BitBufferTest, set33bitValues)
+TEST_F(BitBufferTest, set34bitValues)
 {
-    for (uint32_t cnt = 0; cnt < 33; cnt++) {
+    for (uint32_t cnt = 0; cnt < 34; cnt++) {
         m_testBuffer.appendBits(cnt & 0x1, 1);
     }
 
     uint32_t nrBits;
     uint8_t* pBuffer = m_testBuffer.getReferenceToBuffer(nrBits);
 
-    EXPECT_EQ(33, nrBits);
-    EXPECT_EQ(10, pBuffer[0]);
-    EXPECT_EQ(11, pBuffer[1]);
-    EXPECT_EQ(12, pBuffer[2]);
-    EXPECT_EQ(13, pBuffer[3]);
-    EXPECT_EQ(14, pBuffer[4]);
+    EXPECT_EQ(34, nrBits);
+    EXPECT_EQ(0xaa, pBuffer[0]);
+    EXPECT_EQ(0xaa, pBuffer[1]);
+    EXPECT_EQ(0xaa, pBuffer[2]);
+    EXPECT_EQ(0xaa, pBuffer[3]);
+    EXPECT_EQ(0x02, pBuffer[4]);
+
+    m_testBuffer.restartReading();
+    for (uint32_t cnt = 0; cnt < 34; cnt++) {
+        uint32_t value = m_testBuffer.getBits(1);
+        EXPECT_EQ(value , (cnt & 0x1));
+    }
+}
+
+TEST_F(BitBufferTest, randomBufferReadWrite)
+{
+    typedef struct{
+        uint32_t randVal;
+        uint16_t bitWidth;
+    } RandomNumber;
+    const uint32_t NrValuesInBuffer = 44;
+    RandomNumber randomNumberValues[NrValuesInBuffer];
+
+    // Write random numbers to buffer and store them in an array
+    for (uint16_t cnt = 0; cnt < NrValuesInBuffer; cnt++) {
+        uint32_t value;
+        uint16_t width;
+        getRandomValue(value, width);
+//        std::cout << "Write rand value: " << value << " /  0x" << std::hex << value << std::dec << ", bitWidth: " << width << std::endl;
+        m_testBuffer.appendBits(value, width);
+        randomNumberValues[cnt].randVal = value;
+        randomNumberValues[cnt].bitWidth = width;
+    }
+
+    // Read back values and compare with the values in buffer
+    m_testBuffer.restartReading();
+    for (uint16_t cnt = 0; cnt < NrValuesInBuffer; cnt++) {
+        uint32_t value;
+        uint16_t bitWidth = randomNumberValues[cnt].bitWidth;
+//        std::cout << std::endl << "Read nr Bits: " << bitWidth << std::endl;
+        value = m_testBuffer.getBits(bitWidth);
+
+        uint32_t bitMask = (0x1 << bitWidth) - 1;
+        if (32 == bitWidth) {
+            bitMask = 0xffffffff;
+        }
+
+//        std::cout << "bitMask: 0x" << std::hex << bitMask << std::dec << std::endl;
+//        std::cout << "Value: " << value << " / 0x" << std::hex << value << std::dec << std::endl;
+        EXPECT_EQ((randomNumberValues[cnt].randVal & bitMask), value);
+
+    }
 }
 
 
