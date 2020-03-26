53da14b7350362d508650d788f8980e4c8855b5f
Reto Duss <reto.duss@gmail.com>
2020-03-26 07:10:34 +0100

    Fix bug in backup data script
    Change to the correct directory before accessing files.

--- scripts/backupDataToNAS.sh
+++ scripts/backupDataToNAS.sh
@@ -3,7 +3,7 @@
 # Change into the script directory
 SCRIPTDIR=$(readlink -f $(dirname "$0"))
 pushd "${SCRIPTDIR}" > /dev/null
-
+cd ..
 
 echo "Copy data ..."
 scp HeatingData/*.dat root@192.168.1.100:/volume1/buildingData/heating
