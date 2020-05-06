f8ab34950e8ed480330f15b7ed8c8394bf529394
Reto <reto271@gmx.ch>
2020-05-06 16:09:06 +0200

    Add script to directly monitor my heating.

---
+++ scripts/monitorHeating.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Change into the project root directory
+SCRIPTDIR=$(readlink -f $(dirname "$0"))
+pushd "${SCRIPTDIR}" > /dev/null
+cd ../..
+
+pwd
+../MonitorHeating/bin/MonitorHeating 192.168.1.144 | tee -a ../HeatingData/trace.log
+
+# Back to the original location
+popd > /dev/null
+
+exit ${feedback}
