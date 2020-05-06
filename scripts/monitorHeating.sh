b64c8d7bb18a654f0d2e6fc422cc7998f4ef726e
Reto <reto271@gmx.ch>
2020-05-06 16:29:06 +0200

    Fix path for trace file

--- scripts/monitorHeating.sh
+++ scripts/monitorHeating.sh
@@ -5,7 +5,7 @@ SCRIPTDIR=$(readlink -f $(dirname "$0"))
 pushd "${SCRIPTDIR}" > /dev/null
 cd ..
 
-./MonitorHeating/bin/MonitorHeating 192.168.1.144 | tee -a ../HeatingData/trace.log
+./MonitorHeating/bin/MonitorHeating 192.168.1.144 | tee -a ./HeatingData/trace.log
 
 # Back to the original location
 popd > /dev/null
