d5a64458662cd5834155f04ea3d3e7e009b96bb4
Reto <reto271@gmx.ch>
2020-05-06 16:14:07 +0200

    Fix path in monitorHeating.sh

--- scripts/monitorHeating.sh
+++ scripts/monitorHeating.sh
@@ -3,9 +3,7 @@
 # Change into the project root directory
 SCRIPTDIR=$(readlink -f $(dirname "$0"))
 pushd "${SCRIPTDIR}" > /dev/null
-cd ../..
 
-pwd
 ../MonitorHeating/bin/MonitorHeating 192.168.1.144 | tee -a ../HeatingData/trace.log
 
 # Back to the original location
