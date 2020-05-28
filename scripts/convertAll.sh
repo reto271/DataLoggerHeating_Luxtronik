e3eddf7e6696f354f4d0276640dda83f36a1669f
Reto <reto271@gmx.ch>
2020-05-28 07:50:41 +0200

    Improve convertAll shell script, add error handling
    The script provides a list of files which could no be converted.

--- scripts/convertAll.sh
+++ scripts/convertAll.sh
@@ -8,9 +8,27 @@ echo "::: Build DataDecoder"
 ../MonitorHeating/scripts/build.sh "$0"
 feedback=$?
 
+echo "::: Start Conversion"
 if [ 0 -eq ${feedback} ] ; then
     cd ../
-    find HeatingData/ -name "*.dat" -exec ./DataDecoder/bin/DataDecoder {} \;
+    for dataFile in $(find HeatingData/ -name "*.dat" | sort) ; do
+        ./DataDecoder/bin/DataDecoder ${dataFile}
+        conversionFeedback=$?
+        if  [ 0 -ne  ${conversionFeedback} ] ; then
+            echo "ERROR: conversion file: '${dataFile}' failed"
+            feedback=${conversionFeedback}
+            errorFileList+=( "${dataFile}" )
+        fi
+    done
+fi
+
+# Overall feedback
+echo ""
+if [ 0 -eq ${feedback} ] ; then
+    echo "All files successfuly converted."
+else
+    echo "ERROR: Some Files not succesfully converted"
+    printf '  - %s\n' "${errorFileList[@]}"
 fi
 
 # Back to the original location
