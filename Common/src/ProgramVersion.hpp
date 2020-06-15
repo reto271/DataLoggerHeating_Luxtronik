3c3d69d1a74334b725c7aaa65f585fc07f8db884
Reto Duss <reto.duss@komaxgroup.com>
2020-06-15 07:27:36 +0200

    Documentation for ProgramVersion

--- Common/src/ProgramVersion.hpp
+++ Common/src/ProgramVersion.hpp
@@ -3,15 +3,23 @@
 #include <iostream>
 #include <cstring>
 
+/// Stores the project version and provides the version for all sub-projects. The release process is as follows.
+/// 1) Assume the current released version 1.02
+/// 2) Upon the next change the version is set to 1.03B01
+/// 3) The next change will get version 1.03B02
+/// 4) ...
+/// 5) As soon as the Bxx is stable and working properly the Bxx is removed and set to "" empty string. -> 1.03.
 class ProgramVersion
 {
 public:
+    /// Constructs the version handler
     ProgramVersion();
-    ~ProgramVersion();
 
+    /// Prints the program version along with the provided program name, e.g. MonitorHeating.
+    /// \param[in] programName is the name of the program (sub-project), e.g. DataDecoder, ...
     void printProgramInfo(std::string programName);
 
 private:
-    std::string SoftwareVersion = "V01.03";
-    std::string MinorVersion = "B01";         // format "B01" or empty for release
+    std::string SoftwareVersion = "V01.03";   ///< Major and minor SW version
+    std::string MinorVersion = "B01";         ///< Format "B01" or empty string for release
 };
