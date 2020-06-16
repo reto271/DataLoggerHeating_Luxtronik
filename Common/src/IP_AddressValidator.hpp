acc01497c2cbc6bb7b415c28ac1fff41fb0d393c
Reto <reto271@gmx.ch>
2020-06-16 10:21:37 +0200

    Add doxy comments and unit test for the IP Address Validator

--- Common/src/IP_AddressValidator.hpp
+++ Common/src/IP_AddressValidator.hpp
@@ -3,19 +3,32 @@
 #include <string>
 #include <vector>
 
+/// Validates if the given string is a valid IP address. The format must suffice
+///  xxx.xxx.xxx.xxx where xxx = [0..255].
 class IP_AddressValidator
 {
 public:
+    /// Constructor of the IP address validator. The user must provide the string to be validated.
     IP_AddressValidator(std::string ipAddr);
+
+    /// Destructs the IP address validator
     ~IP_AddressValidator();
 
+    /// Validates the IP address. There need to be four blocks separated by dots. Each block must be
+    ///  the range 0..255.
+    /// \return true if the IP Address is valid, false otherwise.
     bool validate();
 private:
     /// check if given string is a numeric string or not
+    /// \param[in] str to be validated as number
+    /// \return true, if the provided string is a number.
     bool isNumber(const std::string& str);
 
     /// Function to split string str using given delimiter
+    /// \param[in] str is a string to be split into several strings.
+    /// \param[in] delim is the character used to split the string.
+    /// \return vector of sub-strings
     std::vector<std::string> split(const std::string& str, char delim);
 
-    std::string m_ipAddr;
+    std::string m_ipAddr;  ///< IP address to be validated as string.
 };
---
+++ Test/src/Test_IpAddressValidator.cpp
@@ -0,0 +1,129 @@
+#include <gtest/gtest.h>
+#include <stdint.h>
+
+#include "Common/src/IP_AddressValidator.hpp"
+
+namespace
+{
+// using ::testing::StrictMock;
+// using ::testing::Return;
+// using ::testing::Exactly;
+// using ::testing::_;
+// using ::testing::HasSubstr;
+
+
+class Test_IpAddressValidator : public testing::Test
+{
+protected:
+    void SetUp() override
+    {
+    }
+
+    void TearDown() override
+    {
+    }
+
+};
+
+
+TEST_F(Test_IpAddressValidator, testSomoeLocalIP)
+{
+    {
+        IP_AddressValidator testIp("192.168.1.0");
+        EXPECT_TRUE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("192.168.1.1");
+        EXPECT_TRUE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("192.168.1.2");
+        EXPECT_TRUE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("192.168.1.3");
+        EXPECT_TRUE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("192.168.1.100");
+        EXPECT_TRUE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("192.168.1.255");
+        EXPECT_TRUE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("255.255.255.0");
+        EXPECT_TRUE(testIp.validate());
+    }
+}
+
+TEST_F(Test_IpAddressValidator, rangeOfIP)
+{
+    for(int w = 0; w < 10; w++) {
+      for(int x = 250; x < 255; x++) {
+          for(int y = 100; y < 110; y++) {
+              for(int z = 0; z < 256; z++) {
+                  std::string ip = std::to_string(w) + "." + std::to_string(x) + "." + std::to_string(y) + "." + std::to_string(z);
+                  // std::cout << "Test IP: " << ip << std::endl;
+                  IP_AddressValidator testIp(ip);
+                  EXPECT_TRUE(testIp.validate());
+              }
+          }
+      }
+    }
+}
+
+TEST_F(Test_IpAddressValidator, outOfRange)
+{
+    {
+        IP_AddressValidator testIp("255.255.255.256");
+        EXPECT_FALSE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("255.255.255.1000");
+        EXPECT_FALSE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("255.255.255.-1");
+        EXPECT_FALSE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("333.333.333.333");
+        EXPECT_FALSE(testIp.validate());
+    }
+
+}
+TEST_F(Test_IpAddressValidator, someInvalidIP)
+{
+    {
+        IP_AddressValidator testIp("255.255.255");
+        EXPECT_FALSE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("255.255.255,0");
+        EXPECT_FALSE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("255.255");
+        EXPECT_FALSE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("255.255.255..200");
+        EXPECT_FALSE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("255.");
+        EXPECT_FALSE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("255");
+        EXPECT_FALSE(testIp.validate());
+    }
+    {
+        IP_AddressValidator testIp("a.b.c.d");
+        EXPECT_FALSE(testIp.validate());
+    }
+}
+
+}  // unnamed namespace
