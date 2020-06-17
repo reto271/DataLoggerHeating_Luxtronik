ee044c08ef3bacf5cc8b90afc9ed664c8539ad37
Reto <reto271@gmx.ch>
2020-06-17 08:04:40 +0200

    Doxygen comments for the CSV data writer files.

--- DataDecoder/src/FileDataWriterCSV.hpp
+++ DataDecoder/src/FileDataWriterCSV.hpp
@@ -6,17 +6,23 @@
 
 #include "DataDecoder/src/IFileDataWriterCSV.hpp"
 
+/// Writes CSV files with the provided data. The data structure how the data must be provides is defined
+///  in IFileDataWriterCSV.
 class FileDataWriterCSV : public IFileDataWriterCSV
 {
 public:
+    /// Constructs a CSV file writer object.
+    /// \param[in] fileName is the name of the file the CSV data shall be written to.
     FileDataWriterCSV(std::string fileName);
+
+    /// Closes the CSV file and tears-down the object.
     virtual ~FileDataWriterCSV();
 
     bool writeHeader(std::string headerLine) override;
     bool writeData(std::vector<DataEntryCSV> dataVector, const uint32_t nrColumnExclTimeStamp) override;
 
 private:
-    std::string m_fileName;
-    std::ofstream m_csvFile;
-    bool m_fileIsEmpty;
+    std::string m_fileName;    ///< File name of the CSV file.
+    std::ofstream m_csvFile;   ///< File stream to write the data to.
+    bool m_fileIsEmpty;        ///< True if the file is empty.
 };
--- DataDecoder/src/IFileDataWriterCSV.hpp
+++ DataDecoder/src/IFileDataWriterCSV.hpp
@@ -8,16 +8,29 @@
 
 #include "Common/src/CommonTypeDefs.hpp"
 
+/// Interface class for the FileDataWriterCSV. The class writes CSV files with the provided data.
 class IFileDataWriterCSV
 {
 public:
+    /// The user must provide the data to be written in a CSV file using a vector of this
+    ///  struct.
     typedef struct {
-        std::variant<uint32_t, int32_t> uiVal;
-        uint32_t divisor;
+        std::variant<uint32_t, int32_t> uiVal;  ///< Raw value
+        uint32_t divisor;                       ///< Divisor. CSV value = (raw value) / divisor
     } DataEntryCSV;
 
-//    virtual ~IFileDataWriterCSV(){}
+    /// Header line to be written to the CSV.
+    /// \remark The function might be called multiple times, if there shall be more than one row heading
+    ///   data, e.g. the first now is the data name and the second is the units.
+    /// \param[in] headerLine contains the column headings, the headings must be separated by comma.
+    /// \return true if the header line could be written to the file.
     virtual bool writeHeader(std::string headerLine) = 0;
+
+    /// Writes the data to the CSV file. It starts a new row after (1 + nrColumnExclTimeStamp).
+    /// \param[in] dataVector is a Nx1 data vector of all data.
+    /// \param[in] nrColumnExclTimeStamp determines after how may values from dataVector a line
+    ///    break is introduced. A line break is added after (1 + nrColumnExclTimeStamp).
+    /// \return true if the header line could be written to the file.
     virtual bool writeData(std::vector<DataEntryCSV> dataVector, const uint32_t nrColumnExclTimeStamp) = 0;
 };
 
