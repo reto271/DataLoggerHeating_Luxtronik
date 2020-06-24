a23380594a604e388d604434b8c26337cfb1418d
Reto <reto271@gmx.ch>
2020-06-24 17:11:44 +0200

    Fix platform dependent unit test.

--- Test/src/Test_influxdb.cpp
+++ Test/src/Test_influxdb.cpp
@@ -112,7 +112,12 @@ TEST_F(Test_influxdb, writeToTestDB)
     printIOVec(reqInfo);
 
     // Check that response is success
-    std::string expectedHeader = "POST /write?db=heatingdb_test&u=user_test&p=password_test&epoch=ms HTTP/1.1\r\nHost: 192.168.1.100\r\nContent-Length: 64\r\n\r\n";
+    std::string expectedHeader;
+    if (8 == sizeof(std::time_t)) {
+        expectedHeader = "POST /write?db=heatingdb_test&u=user_test&p=password_test&epoch=ms HTTP/1.1\r\nHost: 192.168.1.100\r\nContent-Length: 64\r\n\r\n";
+    } else {
+        expectedHeader = "POST /write?db=heatingdb_test&u=user_test&p=password_test&epoch=ms HTTP/1.1\r\nHost: 192.168.1.100\r\nContent-Length: 54\r\n\r\n";
+    }
     std::string expectedBody = "heating_data_test,unit=kWh TotalEnergy=100.5 " + std::to_string(testTime);
     EXPECT_STREQ(reqInfo.header.c_str(), expectedHeader.c_str());
     EXPECT_STREQ(reqInfo.body.c_str(), expectedBody.c_str());
